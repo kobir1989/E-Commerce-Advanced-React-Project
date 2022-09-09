0b3ef6c4992de3a78853aa3de59d814b0a8eb012
Kabir Hossain <104571104+kobir1989@users.noreply.github.com>
2022-09-09 17:36:49 +0600

    Landding page Path changed

--- src/App.js
+++ src/App.js
@@ -1,47 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Hero from './Components/LandingPage/Header/Hero/Hero';
-import NavBar from './Components/LandingPage/Header/NavBar/NavBar';
-import InfoBanner from './Components/LandingPage/Banner/InfoBanner';
-import CategoryBanner from './Components/LandingPage/Banner/CategoryBanner';
-import BlackFridayBanner from './Components/LandingPage/Banner/BlackFridayBanner';
-import DealsOfTheDay from './Components/LandingPage/Product/DealsOfTheDay';
-import SaleBanner from './Components/LandingPage/Banner/SaleBanner';
-import NewArrival from './Components/LandingPage/Product/NewArrival';
-import ExclusiveBanner from './Components/LandingPage/Banner/ExclusiveBanner';
-import BestSeller from './Components/LandingPage/Product/BestSeller';
-import Footer from './Components/LandingPage/Footer/Footer';
-import Cart from './Components/LandingPage/Cart/Cart';
+import LandingPage from './Components/LandingPage/LandingPage';
 const App = () => {
-  const [showCart, setShowCart] = useState(false);
-  const showCartHandler = () => {
-    setShowCart(true);
-  };
-  const closeCartHandler = () => {
-    setShowCart(!showCart);
-  };
-  return (
-    <>
-      {showCart && <Cart onCloseCart={closeCartHandler} />}
-      <header>
-        <NavBar onShowCart={showCartHandler} />
-        <Hero />
-      </header>
-      <main>
-        <InfoBanner />
-        <CategoryBanner />
-        <BlackFridayBanner />
-        <DealsOfTheDay />
-        <SaleBanner />
-        <NewArrival />
-        <ExclusiveBanner />
-        <BestSeller />
-      </main>
-      <footer>
-        <Footer />
-      </footer>
-    </>
-  );
+  return <LandingPage />;
 };
 
 export default App;
