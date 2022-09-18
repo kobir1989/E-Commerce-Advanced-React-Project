3cfdc07289150d48bac2c68b1596241170df3cb4
Kabir Hossain <104571104+kobir1989@users.noreply.github.com>
2022-09-19 00:06:03 +0600

    Loader and Error Message implimented in the fetch request component

--- src/Components/FetchFromAPI/FetchProductsFromAPI.js
+++ src/Components/FetchFromAPI/FetchProductsFromAPI.js
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { v4 } from 'uuid';
 import ProductRenderBody from './ProductRenderBody';
+import Icons from '../../Utils/Icons';
 const FetchProductsFromAPI = () => {
   const [product, setProduct] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const getProduct = async () => {
-    const response = await fetch('https://fakestoreapi.com/products');
-    const data = await response.json();
-    const finalData = data.map((item) => ({
-      title: item.title.slice(0, 22),
-      price: item.price,
-      img: item.image,
-      id: v4(),
-      params: item.id,
-      description: item.description,
-    }));
-    setProduct(finalData);
+    setIsError(null);
+    setIsLoading(true);
+    try {
+      const response = await fetch('https://fakestoreapi.com/products');
+      if (!response.ok) {
+        throw new Error(isError);
+      }
+      const data = await response.json();
+      const finalData = data.map((item) => ({
+        title: item.title.slice(0, 22),
+        price: item.price,
+        img: item.image,
+        id: v4(),
+        params: item.id,
+        description: item.description,
+      }));
+      setProduct(finalData);
+    } catch (error) {
+      setIsError(error);
+    }
+    setIsLoading(false);
   };
   useEffect(() => {
     getProduct();
@@ -23,6 +36,16 @@ const FetchProductsFromAPI = () => {
     <>
       <div className='flex items-center justify-center'>
         <ProductRenderBody product={product} />
+        {isError && (
+          <p className='text-red font-medium text-[1.1rem]'>
+            Status 404 Something went wrong!
+          </p>
+        )}
+        {isLoading && (
+          <span className='animate-spin text-[4rem] text-red'>
+            <Icons name={'Loader'} />
+          </span>
+        )}
       </div>
     </>
   );
