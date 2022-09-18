968d387bfc1f56a1c9f86f99e5347cc04e2d4661
Kabir Hossain <104571104+kobir1989@users.noreply.github.com>
2022-09-18 16:48:16 +0600

    Merging issue fixed

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,13 +1,8 @@
 import React, { useState } from 'react';
 import NavManue from './NavManue';
 import NavSearch from './NavSearch';
-<<<<<<< HEAD
 import StickyNav from './StickyNav';
-const NavBar = ({ onShowCart }) => {
-=======
-
 const NavBar = ({ onShowCart, onOpenWishList }) => {
->>>>>>> test
   const [showCategory, setShowCategory] = useState(true);
   const [showManue, setShowManue] = useState(true);
   const [showUserLogin, setShowUserLogin] = useState(true);
